54611de7e6420a61b937f656215ea2ff0e42110b
yoohaemin <yoohm0630@gmail.com>
2022-01-14 11:58:32 +0900

    hm's

--- src/com/iu/s1/list/ListMain.java
+++ src/com/iu/s1/list/ListMain.java
@@ -4,11 +4,9 @@ public class ListMain {
 	// 실행만 해주는 것
 	public static void main(String[] args) {
 		
-		ListMain lm = new ListMain(); // 만들었던 클래스명으로 사용
+		ListMenu lm = new ListMenu(); // 만들었던 클래스명으로 사용
 		 lm.phoneNember(); // 참조변수명.메서드명
-		
-		
-
+		 
 	}
 
 }
